9187b599e829b38cc9a8b37d8d8e8421fd9576b0
DarrelA <darrelaiscoding@gmail.com>
2023-07-24 17:17:14 +0800

    Small fixes (`todd_mcleod`)

--- todd_mcleod/01-go-modules/hello.go
+++ todd_mcleod/01-go-modules/hello.go
@@ -4,7 +4,7 @@ package hello
 // @rsc probably needs to update their app engine server
 // https://github.com/golang/go/issues/40452
 // https://github.com/rsc/quote/issues/4#issuecomment-665420354
-import "rsc/quote" // Adding a dependency
+import "rsc.io/quote" // Adding a dependency
 
 func Hello() string {
 	println(quote.Hello())
--- todd_mcleod/11-documentation/go.mod
+++ todd_mcleod/11-documentation/go.mod
@@ -2,4 +2,4 @@ module github.com/DarrelASandbox/playground-go/todd_mcleod/11-documentation
 
 go 1.19
 
-require github.com/GoesToEleven/go-programming v0.0.0-20181228215051-5519dbb87bc5 // indirect
+require github.com/GoesToEleven/go-programming v0.0.0-20181228215051-5519dbb87bc5
