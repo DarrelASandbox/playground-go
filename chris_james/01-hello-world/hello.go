a3aab0170e9aeb3cd43b5f7da2e39b1b0dda63e9
DarrelA <darrelaiscoding@gmail.com>
2024-02-13 09:55:09 +0800

    Refactor using `switch statement` & `named return value`

--- chris_james/01-hello-world/hello.go
+++ chris_james/01-hello-world/hello.go
@@ -14,14 +14,30 @@ func Hello(name string, language string) string {
 		name = "World"
 	}
 
-	if language == spanish {
-		return spanishHelloPrefix + name
-	}
-	if language == french {
-		return frenchHelloPrefix + name
+	return greetingPrefix(language) + name
+}
+
+/*
+- In our function signature we have made a named return value `(prefix string)`.
+- This will create a variable called `prefix` in your function.
+  - It will be assigned the "zero" value. This depends on the type, for example `ints` are 0 and for `strings` it is `""`.
+  	- You can return whatever it's set to by just calling `return` rather than `return prefix`.
+  - This will display in the Go Doc for your function so it can make the intent of your code clearer.
+- `default` in the switch case will be branched to if none of the other case statements match.
+- The function name starts with a lowercase letter. In Go, public functions start with a capital letter and private ones start with a lowercase. We don't want the internals of our algorithm to be exposed to the world, so we made this function private.
+- Also, we can group constants in a block instead of declaring them each on their own line. It's a good idea to use a line between sets of related constants for readability.
+*/
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
 
-	return englishHelloPrefix + name
+	return prefix
 }
 
 func main() {
