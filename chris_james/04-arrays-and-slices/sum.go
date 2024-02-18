e2c990291c1df022bfeec791cb56c653edf83095
DarrelA <darrelaiscoding@gmail.com>
2024-02-18 20:14:46 +0800

    Coverage tool

--- chris_james/04-arrays-and-slices/sum.go
+++ chris_james/04-arrays-and-slices/sum.go
@@ -1,9 +1,17 @@
 package sum
 
-func Sum(numbers [5]int) int {
+/*
+The size of an array is encoded in its type.
+If you try to pass an [4]int into a function that expects [5]int, it won't compile.
+
+Go has slices which do not encode the size of the collection and instead can have any size.
+*/
+func Sum(numbers []int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+
+	// Blank identifier
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
 }
--- chris_james/README.md
+++ chris_james/README.md
@@ -7,4 +7,8 @@ godoc -http=:6060 # http://localhost:6060/pkg/
 # Benchmarking
 # Benchmarks are run sequentially.
 go test -bench=.
+
+go test -cover
 ```
+
+It is important to question the value of your tests. It should not be a goal to have as many tests as possible, but rather to have as much confidence as possible in your code base. Having too many tests can turn in to a real problem and it just adds more overhead in maintenance. Every test has a cost.
