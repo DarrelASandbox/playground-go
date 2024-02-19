c7c2621be2677e4334dca5f5c14b785d27a611dd
DarrelA <darrelaiscoding@gmail.com>
2024-02-19 13:19:00 +0800

    `reflect.DeepEqual`

--- chris_james/04-arrays-and-slices/sum_test.go
+++ chris_james/04-arrays-and-slices/sum_test.go
@@ -1,6 +1,9 @@
 package sum
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	// t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -22,6 +25,20 @@ func TestSum(t *testing.T) {
 
 		if got != want {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+	/*
+		  Go does not let you use equality operators with slices.
+			It's important to note that reflect.DeepEqual is not "type safe" -
+			the code will compile even if you did something a bit silly.
+	*/
+	t.Run("sum of all slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := "bob"
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
 		}
 	})
 }
