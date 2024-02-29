ad92576709a03215fc1aa11181fb93780334ca79
DarrelA <darrelaiscoding@gmail.com>
2024-03-01 06:58:56 +0800

    Refactor using an `interface`

--- chris_james/05-structs-methods-interfaces/shapes.go
+++ chris_james/05-structs-methods-interfaces/shapes.go
@@ -2,6 +2,21 @@ package shapes
 
 import "math"
 
+// Interfaces are a very powerful concept in statically typed languages like Go because
+// they allow you to make functions that can be used with different types and
+// create highly-decoupled code whilst still maintaining type-safety.
+
+// In Go interface resolution is implicit.
+// If the type you pass in matches what the interface is asking for, it will compile.
+
+// Notice how our helper does not need to concern itself with whether the shape is a Rectangle or a Circle or a Triangle.
+// By declaring an interface, the helper is decoupled from the concrete types and only has the method it needs to do its job.
+
+// This kind of approach of using interfaces to declare only what you need is very important in software design and will be covered in more detail in later sections.
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
