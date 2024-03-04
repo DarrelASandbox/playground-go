9011567ba50a9bba655eb36f1c5104976a908dcb
DarrelA <darrelaiscoding@gmail.com>
2024-03-04 12:17:18 +0800

    Improve test output

--- chris_james/05-structs-methods-interfaces/shapes_test.go
+++ chris_james/05-structs-methods-interfaces/shapes_test.go
@@ -18,18 +18,24 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 	// anonymous struct
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{Rectangle{12, 6}, 72},
-		{Circle{10}, 314.1592653589793},
-		{Triangle{12, 6}, 36},
+		{name: "Rectangle", shape: Rectangle{12, 6}, hasArea: 72},
+		{name: "Circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12, 6}, hasArea: 36},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				// The %#v format string will print out our struct with the values in its field,
+				// so the developer can see at a glance the properties that are being tested.
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
 	}
 }
--- chris_james/README.md
+++ chris_james/README.md
@@ -9,8 +9,17 @@ godoc -http=:6060 # http://localhost:6060/pkg/
 go test -bench=.
 
 go test -cover
+
+cd 05-structs-methods-interfaces
+go test -run TestArea/Rectangle
 ```
 
 It is important to question the value of your tests. It should not be a goal to have as many tests as possible, but rather to have as much confidence as possible in your code base. Having too many tests can turn in to a real problem and it just adds more overhead in maintenance. Every test has a cost.
 
-- [Table Driven Tests](https://go.dev/wiki/TableDrivenTests)
+Declaring structs to create your own data types which lets you bundle related data together and make the intent of your code clearer
+
+Declaring interfaces so you can define functions that can be used by different types (parametric polymorphism)
+
+Adding methods so you can add functionality to your data types and so you can implement interfaces
+
+[Table driven tests](https://go.dev/wiki/TableDrivenTests) to make your assertions clearer and your test suites easier to extend & maintain
