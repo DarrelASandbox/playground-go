dccba1fc3045fb060e69e9a1eee703ecb2f27048
DarrelA <darrelaiscoding@gmail.com>
2024-03-07 21:27:49 +0800

    Descriptive type

--- chris_james/06-pointers-and-errors/wallet.go
+++ chris_james/06-pointers-and-errors/wallet.go
@@ -2,19 +2,30 @@ package wallet
 
 import "fmt"
 
+type Stringer interface {
+	String() string
+}
+
+type Bitcoin int
+
+// Declare methods on the new type
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
 // In Go if a symbol (variables, types, functions et al) starts with a lowercase symbol then
 // it is private outside the package it's defined in.
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
 // In Go, when you call a function or a method the arguments are copied.
 // Hence, set a pointer to a wallet
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Printf("address of balance in test is %p \n", &w.balance)
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
