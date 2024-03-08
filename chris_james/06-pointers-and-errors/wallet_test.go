efd989c851b70d43689e4ffe27e11b6f604c38fe
DarrelA <darrelaiscoding@gmail.com>
2024-03-08 13:02:29 +0800

    `Withdraw` function

--- chris_james/06-pointers-and-errors/wallet_test.go
+++ chris_james/06-pointers-and-errors/wallet_test.go
@@ -6,15 +6,27 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
-	got := wallet.Balance()
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		got := wallet.Balance()
 
-	fmt.Printf("address of balance in test is %p \n", &wallet.balance)
+		fmt.Printf("address of balance in test is %p \n", &wallet.balance)
 
-	want := Bitcoin(10)
+		want := Bitcoin(10)
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+		got := wallet.Balance()
+		want := Bitcoin(10)
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
