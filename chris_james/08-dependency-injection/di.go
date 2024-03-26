6df5c2a21affb2274c4614fa5b8ee9f931be6820
DarrelA <darrelaiscoding@gmail.com>
2024-03-26 20:46:38 +0800

    Using `fmt.Fprintf` to make use of `Writer`

--- chris_james/08-dependency-injection/di.go
+++ chris_james/08-dependency-injection/di.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 )
 
+// `fmt.Fprintf` is like `fmt.Printf` but instead takes a `Writer` to send the string to,
+// whereas `fmt.Printf` defaults to stdout.
 func Greet(writer *bytes.Buffer, name string) {
-	fmt.Printf("Hello, %s", name)
+	fmt.Fprintf(writer, "Hello, %s", name)
 }
