7debf169afedf3a12aeb1990f3c7da940216c7f2
DarrelA <darrelaiscoding@gmail.com>
2024-03-27 20:58:47 +0800

    Refactor into a more general purpose interface

--- chris_james/08-dependency-injection/di.go
+++ chris_james/08-dependency-injection/di.go
@@ -1,12 +1,18 @@
 package di
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"os"
 )
 
 // `fmt.Fprintf` is like `fmt.Printf` but instead takes a `Writer` to send the string to,
 // whereas `fmt.Printf` defaults to stdout.
-func Greet(writer *bytes.Buffer, name string) {
+// `fmt.Fprintf` allows you to pass in an `io.Writer` which we know both `os.Stdout` and `bytes.Buffer` implement.
+func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func di() {
+	Greet(os.Stdout, "Ellery")
 }
