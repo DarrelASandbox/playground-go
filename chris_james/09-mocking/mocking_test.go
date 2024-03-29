d692095b77b651a455149641a4f5d322225e8c40
DarrelA <darrelaiscoding@gmail.com>
2024-03-30 00:33:27 +0800

    Take a thin slice of functionality and make it work end-to-end, backed by tests

--- chris_james/09-mocking/mocking_test.go
+++ chris_james/09-mocking/mocking_test.go
@@ -9,7 +9,8 @@ func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
 	Countdown(buffer)
 	got := buffer.String()
-	want := "3"
+	want := "3\n2\n1\nGo!"
+
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
