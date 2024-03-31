430d1c74372eb69568afe87cf2c97f46289af4ee
DarrelA <darrelaiscoding@gmail.com>
2024-04-01 01:10:14 +0800

    `Sleeper`, `SpySleeper` & `DefaultSleeper`

--- chris_james/09-mocking/mocking.go
+++ chris_james/09-mocking/mocking.go
@@ -7,17 +7,39 @@ import (
 	"time"
 )
 
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+// Spies are a kind of mock which can record how a dependency is used.
+// They can record the arguments sent in, how many times it has been called, etc.
+// In our case, we're keeping track of how many times Sleep() is called so we can check it in our test.
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 	}
 	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
