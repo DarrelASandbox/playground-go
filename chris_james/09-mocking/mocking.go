f907ac071aea050dbdc5fefd32c53110e3801923
DarrelA <darrelaiscoding@gmail.com>
2024-04-02 01:33:58 +0800

    Intentionally breaking  the test with incorrect sequence

--- chris_james/09-mocking/mocking.go
+++ chris_james/09-mocking/mocking.go
@@ -33,8 +33,10 @@ const countdownStart = 3
 
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
-		fmt.Fprintln(out, i)
 		sleeper.Sleep()
+	}
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
 	}
 	fmt.Fprint(out, finalWord)
 }
