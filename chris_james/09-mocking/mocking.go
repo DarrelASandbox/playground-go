06bc7abd24562e8f3c533316ee2c68871c12c94a
DarrelA <darrelaiscoding@gmail.com>
2024-04-03 15:27:41 +0800

    Handling sleeps that could occur out of sequence

--- chris_james/09-mocking/mocking.go
+++ chris_james/09-mocking/mocking.go
@@ -33,11 +33,15 @@ const countdownStart = 3
 
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
 		sleeper.Sleep()
 	}
-	for i := countdownStart; i > 0; i-- {
-		fmt.Fprintln(out, i)
-	}
+	// for i := countdownStart; i > 0; i-- {
+	// 	sleeper.Sleep()
+	// }
+	// for i := countdownStart; i > 0; i-- {
+	// 	fmt.Fprintln(out, i)
+	// }
 	fmt.Fprint(out, finalWord)
 }
 
