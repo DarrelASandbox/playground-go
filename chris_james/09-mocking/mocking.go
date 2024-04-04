e35b1619d1971f4cf7dfef60c4742c461e4f2e8a
DarrelA <darrelaiscoding@gmail.com>
2024-04-04 15:57:09 +0800

    Extending Sleeper to be configurable

--- chris_james/09-mocking/mocking.go
+++ chris_james/09-mocking/mocking.go
@@ -28,6 +28,23 @@ func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
 
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
 const finalWord = "Go!"
 const countdownStart = 3
 
