5a355c9ebbdb33c8fe6a6af3884e747be50807d1
DarrelA <darrelaiscoding@gmail.com>
2024-04-12 13:42:35 +0800

    Change the tests to use mocks so we have reliable servers to test against that we can control.

--- chris_james/11-racer/racer_test.go
+++ chris_james/11-racer/racer_test.go
@@ -1,10 +1,28 @@
 package racer
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
+// `httptest.NewServer` takes an `http.HandlerFunc` which we are sending in via an anonymous function.
+// `http.HandlerFunc` is a type that looks like this: `type HandlerFunc func(ResponseWriter, *Request)`.
+// We are wrapping it in an httptest.NewServer which makes it easier to use with testing,
+// as it finds an open port to listen on and then you can close it when you're done with your test.
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.dev"
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 
 	want := fastURL
 	got := Racer(slowURL, fastURL)
@@ -12,4 +30,7 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
+
+	slowServer.Close()
+	fastServer.Close()
 }
