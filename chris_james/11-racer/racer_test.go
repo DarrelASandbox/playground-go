82a4d5b38d0c6a74ac2997018d33c97ffbd648e0
DarrelA <darrelaiscoding@gmail.com>
2024-04-13 22:26:09 +0800

    Refactor to reduce duplication

--- chris_james/11-racer/racer_test.go
+++ chris_james/11-racer/racer_test.go
@@ -12,14 +12,8 @@ import (
 // We are wrapping it in an httptest.NewServer which makes it easier to use with testing,
 // as it finds an open port to listen on and then you can close it when you're done with your test.
 func TestRacer(t *testing.T) {
-	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(20 * time.Millisecond)
-		w.WriteHeader(http.StatusOK)
-	}))
-
-	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	}))
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(0 * time.Millisecond)
 
 	slowURL := slowServer.URL
 	fastURL := fastServer.URL
@@ -33,4 +27,11 @@ func TestRacer(t *testing.T) {
 
 	slowServer.Close()
 	fastServer.Close()
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
 }
