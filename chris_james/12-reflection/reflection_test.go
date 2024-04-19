45031cf4d63f9cc8c69a5c23ac752f28cc06ddf6
DarrelA <darrelaiscoding@gmail.com>
2024-04-20 07:51:49 +0800

    Refactor our test into a table based test

--- chris_james/12-reflection/reflection_test.go
+++ chris_james/12-reflection/reflection_test.go
@@ -5,29 +5,43 @@ import (
 	"testing"
 )
 
-// We want to store a slice of strings (`got`) which stores which strings were passed into `fn` by `walk`.
-// Often in previous chapters, we have made dedicated types for this to spy on function/method invocations
-// but in this case, we can just pass in an anonymous function for `fn` that closes over `got`.
+/*
+This test function systematically verifies that the walk function behaves correctly for different structures and values.
+By defining expected outcomes and comparing them with actual results,
+it ensures that walk meets its specification in handling various types of inputs.
+This approach is common in unit testing, where the functionality of individual components is validated in isolation.
+*/
+
+/*
+We're going to be writing a number of tests where we pass in different values and checking the array of strings that `fn` was called with.
+
+We should refactor our test into a table based test to make this easier to continue testing new scenarios.
+*/
 func TestWalk(t *testing.T) {
-	expected := "Chris"
-	var got []string
 
-	// Anonymous struct
-	x := struct {
-		Name string
-	}{expected}
-
-	// Call walk with x and the spy and
-	walk(x, func(input string) {
-		got = append(got, input)
-	})
-
-	if len(got) != 1 {
-		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+	// A slice of anonymous struct instances, each representing a test case.
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"struct with one string field",
+			struct{ Name string }{"Chris"}, []string{"Chris"},
+		},
 	}
 
-	if got[0] != expected {
-		t.Errorf("got %q, want %q", got[0], expected)
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
 	}
 }
 
