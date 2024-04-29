10f1ca080be9f020d01cea9924fbb04b2eba2c9e
DarrelA <darrelaiscoding@gmail.com>
2024-04-29 16:05:50 +0800

    Refactor the code so instead we check the type first and then do our work.

--- chris_james/12-reflection/reflection.go
+++ chris_james/12-reflection/reflection.go
@@ -3,36 +3,31 @@ package reflection
 import "reflect"
 
 /*
-This code is very unsafe and very naive,
-but remember: our goal when we are in "red" (the tests failing) is to write the smallest amount of code possible.
-We then write more tests to address our concerns.
-
 We need to use reflection to have a look at x and try and look at its properties.
 
 make some very optimistic assumptions about the value passed in:
   - We look at the first and only field. However, there may be no fields at all, which would cause a panic.
   - We then call String(), which returns the underlying value as a string.
     However, this would be wrong if the field was something other than a string.
+
+We just have a check at the start to see if it's a slice
+(with a return to stop the rest of the code executing)
+and if it's not we just assume it's a struct.
 */
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
-	if val.Kind() == reflect.Slice {
+	switch val.Kind() {
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walk(val.Field(i).Interface(), fn)
+		}
+	case reflect.Slice:
 		for i := 0; i < val.Len(); i++ {
 			walk(val.Index(i).Interface(), fn)
 		}
-		return
-	}
-
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			walk(field.Interface(), fn)
-		}
+	case reflect.String:
+		fn(val.String())
 	}
 }
 
