d3ebce13ddf7e33ea6c45ed434aa4d2b084422a9
DarrelA <darrelaiscoding@gmail.com>
2024-05-17 16:22:56 +0800

    Managing `Fetch` in a goroutine

--- chris_james/14-context/mycontext.go
+++ chris_james/14-context/mycontext.go
@@ -10,9 +10,27 @@ type Store interface {
 	Cancel()
 }
 
+/*
+`context` has a method `Done()` which returns a channel which gets sent a signal when the context is "done" or "cancelled". We want to listen to that signal and call `store.Cancel` if we get it but we want to ignore it if our `Store` manages to `Fetch` before it.
+
+To manage this we run `Fetch` in a goroutine and it will write the result into a new channel `data`. We then use `select` to effectively race to the two asynchronous processes and then we either write a response or `Cancel`.
+*/
+
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		store.Cancel()
-		fmt.Fprint(w, store.Fetch())
+		ctx := r.Context()
+
+		data := make(chan string, 1)
+
+		go func() {
+			data <- store.Fetch()
+		}()
+
+		select {
+		case d := <-data:
+			fmt.Fprint(w, d)
+		case <-ctx.Done():
+			store.Cancel()
+		}
 	}
 }
