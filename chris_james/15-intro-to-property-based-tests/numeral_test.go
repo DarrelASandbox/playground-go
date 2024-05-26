6dd33848d10a41e0a3e0e5b8ca1eff7c94d13ba6
DarrelA <darrelaiscoding@gmail.com>
2024-05-26 11:26:22 +0800

    Refactor using `strings.Builder`

--- chris_james/15-intro-to-property-based-tests/numeral_test.go
+++ chris_james/15-intro-to-property-based-tests/numeral_test.go
@@ -1,6 +1,9 @@
 package numeral
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestRomanNumerals(t *testing.T) {
 	cases := []struct {
@@ -21,4 +24,15 @@ func TestRomanNumerals(t *testing.T) {
 			}
 		})
 	}
+}
+
+func ConvertToRoman(arabic int) string {
+	// A Builder is used to efficiently build a string using Write methods. It minimizes memory copying.
+	var result strings.Builder
+
+	for i := 0; i < arabic; i++ {
+		result.WriteString("I")
+	}
+
+	return result.String()
 }
