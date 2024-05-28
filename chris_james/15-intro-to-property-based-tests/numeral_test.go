2a9689208ea39418179efdb0a2ebf185a0d41476
DarrelA <darrelaiscoding@gmail.com>
2024-05-28 11:32:48 +0800

    Refactor to fix string building pattern

--- chris_james/15-intro-to-property-based-tests/numeral_test.go
+++ chris_james/15-intro-to-property-based-tests/numeral_test.go
@@ -28,14 +28,14 @@ func TestRomanNumerals(t *testing.T) {
 }
 
 func ConvertToRoman(arabic int) string {
-	if arabic == 4 {
-		return "IV"
-	}
-
 	// A Builder is used to efficiently build a string using Write methods. It minimizes memory copying.
 	var result strings.Builder
 
-	for i := 0; i < arabic; i++ {
+	for i := arabic; i > 0; i-- {
+		if i == 4 {
+			result.WriteString("IV")
+			break
+		}
 		result.WriteString("I")
 	}
 
