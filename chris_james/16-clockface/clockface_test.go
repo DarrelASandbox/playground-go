ed479998ee0db3b16c76b2b6043400bc9226c3a8
DarrelA <darrelaiscoding@gmail.com>
2024-06-12 08:41:07 +0800

    Testing `TestSecondsInRadian`

--- chris_james/16-clockface/clockface_test.go
+++ chris_james/16-clockface/clockface_test.go
@@ -1,18 +1,17 @@
-package clockface_test
+package clockface
 
 import (
+	"math"
 	"testing"
 	"time"
-
-	clockface "github.com/DarrelASandbox/playground-go/chris_james/16-clockface"
 )
 
-func TestSecondHandAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
-	want := clockface.Point{X: 150, Y: 150 - 90}
-	got := clockface.SecondHand(tm)
+func TestSecondsInRadian(t *testing.T) {
+	thirtySeconds := time.Date(312, time.October, 28, 0, 0, 30, 0, time.UTC)
+	want := math.Pi
+	got := secondsInRadian(thirtySeconds)
 
-	if got != want {
-		t.Errorf("Got %v, wanted %v", got, want)
+	if want != got {
+		t.Fatalf("Wanted %v radians, but got %v", want, got)
 	}
 }
