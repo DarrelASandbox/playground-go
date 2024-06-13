e6ce7ffcef1927e587c72be1d172e8efa6b2a2a7
DarrelA <darrelaiscoding@gmail.com>
2024-06-13 08:52:29 +0800

    Implementing table based test

--- chris_james/16-clockface/clockface.go
+++ chris_james/16-clockface/clockface.go
@@ -17,6 +17,8 @@ func SecondHand(t time.Time) Point {
 	return Point{150, 60}
 }
 
+// FAIL: TestSecondsInRadian (0.00s)
+// clockface_test.go:15: Wanted 3.141592653589793 radians, but got 3.1415926535897936
 func secondsInRadian(t time.Time) float64 {
-	return math.Pi
+	return float64(t.Second()) * (math.Pi / 30)
 }
