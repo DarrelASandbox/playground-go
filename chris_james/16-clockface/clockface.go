78c3fc4829584a28bad66591e23afcb61a948058
DarrelA <darrelaiscoding@gmail.com>
2024-06-23 12:19:16 +0800

    Move the hour hand along a little bit for both the minutes and the seconds

--- chris_james/16-clockface/clockface.go
+++ chris_james/16-clockface/clockface.go
@@ -27,8 +27,14 @@ func minuteHandPoint(t time.Time) Point {
 	return angleToPoint(minutesInRadians(t))
 }
 
+/*
+We need to move the hour hand along a little bit for both the minutes and the seconds.
+One full turn is one hour for the minute hand, but for the hour hand it's twelve hours.
+So we just divide the angle returned by minutesInRadians by twelve
+*/
+
 func hoursInRadians(t time.Time) float64 {
-	return (math.Pi / (6 / float64(t.Hour()%12)))
+	return (minutesInRadians(t) / 12) + (math.Pi / (6 / float64(t.Hour()%12)))
 }
 
 func angleToPoint(angle float64) Point {
