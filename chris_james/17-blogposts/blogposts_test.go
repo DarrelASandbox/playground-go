92a9e3b8acfa6129ca4e73eb1f73b0bf7da3eb85
DarrelA <darrelaiscoding@gmail.com>
2024-06-28 18:34:18 +0800

    Change the test data to have the separator, and a body with a few newlines to check we grab all the content

--- chris_james/17-blogposts/blogposts_test.go
+++ chris_james/17-blogposts/blogposts_test.go
@@ -29,10 +29,18 @@ func TestNewBlogPosts(t *testing.T) {
 	const (
 		firstBody = `Title: Post 1
 Description: Description 1
-Tags: tdd, go`
+Tags: tdd, go
+---
+Hello
+World`
+
 		secondBody = `Title: Post 2
 Description: Description 2
-Tags: rust, borrow-checker`
+Tags: rust, borrow-checker
+---
+B
+L
+M`
 	)
 
 	fs := fstest.MapFS{
@@ -55,6 +63,8 @@ Tags: rust, borrow-checker`
 		Title:       "Post 1",
 		Description: "Description 1",
 		Tags:        []string{"tdd", "go"},
+		Body: `Hello
+World`,
 	})
 }
 
