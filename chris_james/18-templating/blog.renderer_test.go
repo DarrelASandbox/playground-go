4fb3b110e269c213d3492c34ee54f9c35d22d0a1
DarrelA <darrelaiscoding@gmail.com>
2024-06-29 21:49:37 +0800

    `18-templating`: `TestReader`

---
+++ chris_james/18-templating/blog.renderer_test.go
@@ -0,0 +1,34 @@
+package renderer_test
+
+import (
+	"bytes"
+	"testing"
+
+	blog_renderer "github.com/DarrelASandbox/playground-go/chris_james/18-templating"
+)
+
+func TestReader(t *testing.T) {
+	var (
+		aPost = blog_renderer.Post{
+			Title:       "hello world",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	t.Run("it converts a single post into HTML", func(t *testing.T) {
+		buf := bytes.Buffer{}
+		err := blog_renderer.Render(&buf, aPost)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		got := buf.String()
+		want := `<h1>hello world</h1>`
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
+	})
+}
---
+++ chris_james/18-templating/go.mod
@@ -0,0 +1,3 @@
+module github.com/DarrelASandbox/playground-go/chris_james/18-templating
+
+go 1.21
