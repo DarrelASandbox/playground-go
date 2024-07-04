e9b8f73c65c50abfb48ce828136e648d655e8298
DarrelA <darrelaiscoding@gmail.com>
2024-07-04 09:47:29 +0800

    Using type `interface{}`

--- chris_james/19-generics/assert.go
+++ chris_james/19-generics/assert.go
@@ -2,14 +2,20 @@ package generics
 
 import "testing"
 
-func AssertEqual(t *testing.T, got, want int) {
+/*
+Ideally, we don't want to have to make specific AssertX functions for every type we ever deal with.
+We'd like to be able to have one AssertEqual function that works with any type but
+does not let you compare apples and oranges.
+*/
+
+func AssertEqual(t *testing.T, got, want interface{}) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %d, want %d", got, want)
 	}
 }
 
-func AssertNotEqual(t *testing.T, got, want int) {
+func AssertNotEqual(t *testing.T, got, want interface{}) {
 	t.Helper()
 	if got == want {
 		if got == want {
