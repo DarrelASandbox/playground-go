70ff2eb6d65a2203003b86ccc4e2852d7c963eb3
DarrelA <darrelaiscoding@gmail.com>
2024-07-07 13:01:25 +0800

    Improve the ergonomics of the bank code

--- chris_james/20-generics-and-arrays/bad_bank.go
+++ chris_james/20-generics-and-arrays/bad_bank.go
@@ -5,18 +5,37 @@ type Transaction struct {
 	Sum      float64
 }
 
-func BalanceFor(transactions []Transaction, name string) float64 {
-	adjustBalance := func(currentBalance float64, t Transaction) float64 {
-		if t.From == name {
-			return currentBalance - t.Sum
-		}
+func NewTransaction(from, to Account, sum float64) Transaction {
+	return Transaction{From: from.Name, To: to.Name, Sum: sum}
+}
 
-		if t.To == name {
-			return currentBalance + t.Sum
-		}
+type Account struct {
+	Name    string
+	Balance float64
+}
 
-		return currentBalance
+/*
+This demonstrates the power of using concepts like `Reduce`.
+The `NewBalanceFor` function appears more declarative, describing what happens rather than how it happens.
+Often, when reading code, developers are navigating through numerous files,
+attempting to understand what is happening rather than how it is implemented,
+and this coding style supports that process effectively.
+
+If a developer wishes to delve into the details,
+they can do so and observe the business logic of `applyTransaction`
+without concerning themselves with loops and mutating state; Reduce handles that separately.
+*/
+func NewBalanceFor(account Account, transactions []Transaction) Account {
+	return Reduce(transactions, applyTransaction, account)
+}
+
+func applyTransaction(a Account, transaction Transaction) Account {
+	if transaction.From == a.Name {
+		a.Balance -= transaction.Sum
+	}
+	if transaction.To == a.Name {
+		a.Balance += transaction.Sum
 	}
 
-	return Reduce(transactions, adjustBalance, 0.0)
+	return a
 }
