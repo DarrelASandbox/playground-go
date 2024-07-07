35ae335c57ec8af4d81add1fcd004b2c8fabc201
DarrelA <darrelaiscoding@gmail.com>
2024-07-07 12:48:21 +0800

    Reduce to a different type than the type of the collection

--- chris_james/20-generics-and-arrays/sum.go
+++ chris_james/20-generics-and-arrays/sum.go
@@ -36,8 +36,11 @@ func SumAllTails(numbers ...[]int) []int {
 `Reduce` captures the essence of the pattern, it's a function that takes a collection,
 an accumulating function, an initial value, and returns a single value.
 There's no messy distractions around concrete types.
+
+We've added a second type constraint which has allowed us to loosen the constraints on `Reduce`.
+This allows people to `Reduce` from a collection of `A` into a `B`. In our case from `Transaction` to `float64`.
 */
-func Reduce[A any](collection []A, f func(A, A) A, initialValue A) A {
+func Reduce[A, B any](collection []A, f func(B, A) B, initialValue B) B {
 	var result = initialValue
 	for _, x := range collection {
 		result = f(result, x)
