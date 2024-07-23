fc4f7bc41d2516a523e36a74c561d895f92ff146
DarrelA <darrelaiscoding@gmail.com>
2024-07-23 11:38:05 +0800

    Add new unit test to `TestGreet`

--- chris_james/02-testing-fundamentals/02-specs-greet/domain/interactions/greet_test.go
+++ chris_james/02-testing-fundamentals/02-specs-greet/domain/interactions/greet_test.go
@@ -5,8 +5,13 @@ import (
 
 	"github.com/DarrelASandbox/playground-go/chris_james/02-testing-fundamentals/specs-greet/domain/interactions"
 	"github.com/DarrelASandbox/playground-go/chris_james/02-testing-fundamentals/specs-greet/specifications"
+	"github.com/alecthomas/assert/v2"
 )
 
 func TestGreet(t *testing.T) {
 	specifications.GreetSpecification(t, specifications.GreetAdapter(interactions.Greet))
+
+	t.Run("default name to world if it's an empty string", func(t *testing.T) {
+		assert.Equal(t, "Hello, World", interactions.Greet(""))
+	})
 }
