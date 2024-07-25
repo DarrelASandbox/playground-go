9befb0afa39828ebfd12fcda4ff42d3c0a0d00e2
DarrelA <darrelaiscoding@gmail.com>
2024-07-25 12:37:45 +0800

    Move gRPC `GreetServer` to `server.go`

--- chris_james/02-testing-fundamentals/02-specs-greet/cmd/grpcserver/main.go
+++ chris_james/02-testing-fundamentals/02-specs-greet/cmd/grpcserver/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net"
 
 	"github.com/DarrelASandbox/playground-go/chris_james/02-testing-fundamentals/specs-greet/adapters/grpcserver"
-	"github.com/DarrelASandbox/playground-go/chris_james/02-testing-fundamentals/specs-greet/domain/interactions"
 	"google.golang.org/grpc"
 )
 
@@ -17,20 +15,8 @@ func main() {
 	}
 
 	s := grpc.NewServer()
-	grpcserver.RegisterGreeterServer(s, &GreetServer{})
+	grpcserver.RegisterGreeterServer(s, &grpcserver.GreetServer{})
 	if err := s.Serve(lis); err != nil {
 		log.Fatal(err)
 	}
-}
-
-type GreetServer struct {
-	grpcserver.UnimplementedGreeterServer
-}
-
-func (g GreetServer) Greet(ctx context.Context, request *grpcserver.GreetRequest) (*grpcserver.GreetReply, error) {
-	return &grpcserver.GreetReply{Message: interactions.Greet(request.Name)}, nil
-}
-
-func (g GreetServer) Curse(ctx context.Context, request *grpcserver.CurseRequest) (*grpcserver.CurseReply, error) {
-	return &grpcserver.CurseReply{Message: interactions.Curse(request.Name)}, nil
 }
