f8d21ffcb03cdc1a9ffd0a510622ab119400ad11
DarrelA <darrelaiscoding@gmail.com>
2024-07-26 09:53:25 +0800

    `greetPath` & `cursePath` handlers

--- chris_james/02-testing-fundamentals/02-specs-greet/adapters/webserver/handler.go
+++ chris_james/02-testing-fundamentals/02-specs-greet/adapters/webserver/handler.go
@@ -4,6 +4,13 @@ import (
 	"embed"
 	"html/template"
 	"net/http"
+
+	"github.com/DarrelASandbox/playground-go/chris_james/02-testing-fundamentals/specs-greet/domain/interactions"
+)
+
+const (
+	greetPath = "/greet"
+	cursePath = "/curse"
 )
 
 //go:embed markup/*.gohtml
@@ -22,9 +29,24 @@ func NewHandler() (http.Handler, error) {
 	handler := handler{templ: templ}
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handler.form)
+	mux.HandleFunc(greetPath, handler.replyWith(interactions.Greet))
+	mux.HandleFunc(cursePath, handler.replyWith(interactions.Curse))
 	return mux, nil
 }
 
 func (h handler) form(w http.ResponseWriter, _ *http.Request) {
 	_ = h.templ.ExecuteTemplate(w, "form.gohtml", nil)
+}
+
+func (h handler) replyWith(interact func(name string) string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		if err := h.templ.ExecuteTemplate(w, "reply.gohtml", interact(r.Form.Get("name"))); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
 }
