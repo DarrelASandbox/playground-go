2f355dfa90fe27a132927b7b9774c0ed61378afc
DarrelA <darrelaiscoding@gmail.com>
2024-08-02 22:18:02 +0800

    `TestRecipePlanner`: Added more tests

--- chris_james/02-testing-fundamentals/03-fakes-and-contracts/domain/planner/planner_test.go
+++ chris_james/02-testing-fundamentals/03-fakes-and-contracts/domain/planner/planner_test.go
@@ -2,10 +2,12 @@ package planner_test
 
 import (
 	"context"
+	"errors"
 	"testing"
 	"time"
 
 	"github.com/DarrelASandbox/playground-go/chris_james/02-testing-fundamentals/fakes-and-contracts/adapters/driven/persistence/inmemory"
+	"github.com/DarrelASandbox/playground-go/chris_james/02-testing-fundamentals/fakes-and-contracts/domain/ingredients"
 	"github.com/DarrelASandbox/playground-go/chris_james/02-testing-fundamentals/fakes-and-contracts/domain/planner"
 	"github.com/DarrelASandbox/playground-go/chris_james/02-testing-fundamentals/fakes-and-contracts/domain/planner/internal/expect"
 	"github.com/DarrelASandbox/playground-go/chris_james/02-testing-fundamentals/fakes-and-contracts/domain/planner/internal/plannertest"
@@ -51,12 +53,74 @@ func (r RecipePlannerTest) Test(t *testing.T) {
 			expect.Equal(t, 0, len(remainingIngredients))
 		})
 
-		t.Run("returns a missing ingredients error if you try to schedule a meal without all the ingredients", func(t *testing.T) {})
-		t.Run("when recipeBook fails to get ingredients, we get an error", func(t *testing.T) {})
-		t.Run("returns the specific ingredients missing if you try to schedule a meal with some missing ingredients", func(t *testing.T) {})
+		t.Run("returns a missing ingredients error if you try to schedule a meal without all the ingredients",
+			func(t *testing.T) {
+				var (
+					ctx                          = context.Background()
+					lasagna                      = plannertest.RandomRecipe()
+					recipeBook, pantry, teardown = r.CreateDependencies()
+					sut                          = planner.New(recipeBook, pantry)
+				)
+
+				t.Cleanup(teardown)
+
+				expect.NoErr(t, recipeBook.AddRecipes(ctx, lasagna))
+				err := sut.ScheduleMeal(ctx, lasagna, time.Now())
+				expect.Err(t, err)
+				missingIngredientsErr, ok := err.(planner.ErrorMissingIngredients)
+				expect.True(t, ok)
+				expect.DeepEqual(t, planner.ErrorMissingIngredients{
+					MissingIngredients: lasagna.Ingredients}, missingIngredientsErr)
+			})
+
+		t.Run("when recipeBook fails to get ingredients, we get an error", func(t *testing.T) {
+			var (
+				ctx                          = context.Background()
+				lasagna                      = plannertest.RandomRecipe()
+				recipeBook, pantry, teardown = r.CreateDependencies()
+				failingPantry                = planner.NewPantryDelegate(pantry)
+			)
+
+			t.Cleanup(teardown)
+
+			failingPantry.GetIngredientsFunc = func(ctx context.Context) (ingredients.Ingredients, error) {
+				return nil, errors.New("oh no")
+			}
+
+			sut := planner.New(recipeBook, failingPantry)
+			expect.NoErr(t, recipeBook.AddRecipes(ctx, lasagna))
+			expect.NoErr(t, pantry.Store(ctx, lasagna.Ingredients...))
+
+			err := sut.ScheduleMeal(ctx, lasagna, time.Now())
+			expect.Err(t, err)
+		})
+
+		t.Run("returns the specific ingredients missing if you try to schedule a meal with some missing ingredients",
+			func(t *testing.T) {
+				var (
+					ctx                          = context.Background()
+					lasagna                      = plannertest.RandomRecipe()
+					recipeBook, pantry, teardown = r.CreateDependencies()
+					sut                          = planner.New(recipeBook, pantry)
+				)
+
+				t.Cleanup(teardown)
+
+				expect.NoErr(t, recipeBook.AddRecipes(ctx, lasagna))
+				missingIngredient, ingredientsWeHave := lasagna.Ingredients[0], lasagna.Ingredients[1:]
+				expect.NoErr(t, pantry.Store(ctx, ingredientsWeHave...))
+				err := sut.ScheduleMeal(ctx, lasagna, time.Now())
+				expect.Err(t, err)
+				missingIngredientsErr, ok := err.(planner.ErrorMissingIngredients)
+				expect.True(t, ok)
+				expect.DeepEqual(t, planner.ErrorMissingIngredients{
+					MissingIngredients: []ingredients.Ingredient{missingIngredient},
+				}, missingIngredientsErr)
+			})
 	})
 
 	t.Run("suggesting recipes", func(t *testing.T) {
+		t.Run("if don't have the ingredients for a meal, we can't make it", func(t *testing.T) {})
 		t.Run("if we have the ingredients for a recipe we can make it", func(t *testing.T) {})
 		t.Run("if we have ingredients for 2 recipes, we can make both", func(t *testing.T) {})
 	})
