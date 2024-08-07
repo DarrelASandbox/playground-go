e44abe4b194eb0b64c214b6385b273bda0a5a0f0
DarrelA <darrelaiscoding@gmail.com>
2024-08-07 11:44:09 +0800

    Refactor: Add `StubPlayerStore` & `PlayerStore`

--- chris_james/03-build-an-app/01-http-server/server_test.go
+++ chris_james/03-build-an-app/01-http-server/server_test.go
@@ -7,20 +7,32 @@ import (
 	"testing"
 )
 
+type StubPlayerStore struct {
+	scores map[string]int
+}
+
 func TestGETPlayer(t *testing.T) {
+	store := StubPlayerStore{map[string]int{"Pepper": 20, "Floyd": 10}}
+	server := &PlayerServer{&store}
+
 	t.Run("returns Pepper's score", func(t *testing.T) {
 		request := newGetScoreRequest("Pepper")
 		response := httptest.NewRecorder()
-		PlayerServer(response, request)
+		server.ServeHTTP(response, request)
 		assertResponseBody(t, response.Body.String(), "20")
 	})
 
 	t.Run("returns Floyd's score", func(t *testing.T) {
 		request := newGetScoreRequest("Floyd")
 		response := httptest.NewRecorder()
-		PlayerServer(response, request)
+		server.ServeHTTP(response, request)
 		assertResponseBody(t, response.Body.String(), "10")
 	})
+}
+
+func (s *StubPlayerStore) GetPlayerScore(name string) int {
+	score := s.scores[name]
+	return score
 }
 
 func newGetScoreRequest(name string) *http.Request {
