f75b002e7cfd1c296817bd84db3d63c27a24d9e5
DarrelA <darrelaiscoding@gmail.com>
2024-08-10 15:43:05 +0800

    `TestLeague`

--- chris_james/03-build-an-app/02-json-routing-embedding/server_test.go
+++ chris_james/03-build-an-app/02-json-routing-embedding/server_test.go
@@ -61,6 +61,18 @@ func TestStoreWins(t *testing.T) {
 	})
 }
 
+func TestLeague(t *testing.T) {
+	store := StubPlayerStore{}
+	server := &PlayerServer{&store}
+
+	t.Run("it returns 200 on /league", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/league", nil)
+		response := httptest.NewRecorder()
+		server.ServeHTTP(response, request)
+		assertStatus(t, response.Code, http.StatusOK)
+	})
+}
+
 func (s *StubPlayerStore) GetPlayerScore(name string) int {
 	score := s.scores[name]
 	return score
