bd0eb33422ddbe4d71b904a3d5eabd1899aec584
DarrelA <darrelaiscoding@gmail.com>
2024-08-13 17:45:53 +0800

    Intended failing test when reading again

--- chris_james/03-build-an-app/03-io-sorting/file_system_store_test.go
+++ chris_james/03-build-an-app/03-io-sorting/file_system_store_test.go
@@ -15,5 +15,14 @@ func TestFileSystemStore(t *testing.T) {
 		got := store.GetLeague()
 		want := []Player{{"Cleo", 10}, {"Chris", 33}}
 		assertLeague(t, got, want)
+
+		/*
+		   - read again
+		   - we want this to pass, but if you run the test it doesn't
+		   - the problem is our `Reader` has reached the end so there is nothing more to read.
+		   - We need a way to tell it to go back to the start
+		*/
+		got = store.GetLeague()
+		assertLeague(t, got, want)
 	})
 }
