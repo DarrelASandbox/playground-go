87c3be681065a12b5c2364b3438b701a7e49493a
DarrelA <darrelaiscoding@gmail.com>
2024-08-13 17:26:58 +0800

    `03-io-sorting`: Starter files from previous section

---
+++ chris_james/03-build-an-app/03-io-sorting/go.mod
@@ -0,0 +1,3 @@
+module github.com/DarrelASandbox/playground-go/chris_james/03-build-an-app/io-sorting
+
+go 1.23.0
---
+++ chris_james/03-build-an-app/03-io-sorting/in_memory_player_store.go
@@ -0,0 +1,37 @@
+package main
+
+import "sync"
+
+// NewInMemoryPlayerStore initialize an empty player store.
+func NewInMemoryPlayerStore() *InMemoryPlayerStore {
+	return &InMemoryPlayerStore{map[string]int{}, sync.RWMutex{}}
+}
+
+// InMemoryPlayerStore collects data about players in memory.
+type InMemoryPlayerStore struct {
+	store map[string]int
+	lock  sync.RWMutex // A mutex is used to synchronize read/write access to the map
+}
+
+// GetPlayerScore retrieves scores for a given player
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	i.lock.RLock()
+	defer i.lock.RUnlock()
+	return i.store[name]
+}
+
+// RecordWin will record a player's win.
+func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.lock.Lock()
+	defer i.lock.Unlock()
+	i.store[name]++
+}
+
+// GetLeague currently doesn't work, but it should return the player league.
+func (i *InMemoryPlayerStore) GetLeague() []Player {
+	var league []Player
+	for name, wins := range i.store {
+		league = append(league, Player{name, wins})
+	}
+	return league
+}
