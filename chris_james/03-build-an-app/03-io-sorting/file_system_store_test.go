bac1f0dbe7f34c73bbf9ed6ef4ecd419ef8f2bcb
DarrelA <darrelaiscoding@gmail.com>
2024-08-14 18:11:17 +0800

    Switch from `ReadSeeker` to `ReadWriteSeeker` & `createTempFile`

--- chris_james/03-build-an-app/03-io-sorting/file_system_store_test.go
+++ chris_james/03-build-an-app/03-io-sorting/file_system_store_test.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"strings"
+	"io"
+	"os"
 	"testing"
 )
 
 func TestFileSystemStore(t *testing.T) {
-	database := strings.NewReader(`[
+	database, cleanDatabase := createTempFile(t, `[
     {"Name": "Cleo", "Wins": 10},
     {"Name": "Chris", "Wins": 33}]`)
+	defer cleanDatabase()
 
 	store := FileSystemPlayerStore{database}
 
@@ -26,6 +28,22 @@ func TestFileSystemStore(t *testing.T) {
 		want := 33
 		assertScoreEquals(t, got, want)
 	})
+}
+
+func createTempFile(t testing.TB, initialData string) (io.ReadWriteSeeker, func()) {
+	t.Helper()
+	tmpfile, err := os.CreateTemp("", "db")
+	if err != nil {
+		t.Fatalf("could not create temp file %v", err)
+	}
+
+	tmpfile.Write([]byte(initialData))
+	removeFile := func() {
+		tmpfile.Close()
+		os.Remove(tmpfile.Name())
+	}
+
+	return tmpfile, removeFile
 }
 
 func assertScoreEquals(t testing.TB, got, want int) {
