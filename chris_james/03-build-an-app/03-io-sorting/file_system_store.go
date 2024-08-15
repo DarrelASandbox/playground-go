f5871a610cdc621a3245500954c6b6eb0b55bf3f
DarrelA <darrelaiscoding@gmail.com>
2024-08-15 11:09:18 +0800

    Refactor: `NewFileSystemPlayerStore`

--- chris_james/03-build-an-app/03-io-sorting/file_system_store.go
+++ chris_james/03-build-an-app/03-io-sorting/file_system_store.go
@@ -5,18 +5,30 @@ import (
 	"io"
 )
 
+/*
+Every time someone calls GetLeague() or GetPlayerScore() we are reading the entire file and parsing it into JSON.
+We should not have to do that because FileSystemStore is entirely responsible for the state of the league;
+it should only need to read the file when the program starts up and only need to update the file when data changes.
+
+We can create a constructor which can do some of this initialization for us and store the league as a value in our FileSystemStore to be used on the reads instead.
+*/
 type FileSystemPlayerStore struct {
 	database io.ReadWriteSeeker
+	league   League
+}
+
+func NewFileSystemPlayerStore(database io.ReadWriteSeeker) *FileSystemPlayerStore {
+	database.Seek(0, io.SeekStart)
+	league, _ := NewLeague(database)
+	return &FileSystemPlayerStore{database, league}
 }
 
 func (f *FileSystemPlayerStore) GetLeague() League {
-	f.database.Seek(0, io.SeekStart)
-	league, _ := NewLeague(f.database)
-	return league
+	return f.league
 }
 
 func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
-	player := f.GetLeague().Find(name)
+	player := f.league.Find(name)
 	if player != nil {
 		return player.Wins
 	}
@@ -24,14 +36,13 @@ func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
 }
 
 func (f *FileSystemPlayerStore) RecordWin(name string) {
-	league := f.GetLeague()
-	player := league.Find(name)
+	player := f.league.Find(name)
 	if player != nil {
 		player.Wins++
 	} else {
-		league = append(league, Player{name, 1})
+		f.league = append(f.league, Player{name, 1})
 	}
 
 	f.database.Seek(0, io.SeekStart)
-	json.NewEncoder(f.database).Encode(league)
+	json.NewEncoder(f.database).Encode(f.league)
 }
