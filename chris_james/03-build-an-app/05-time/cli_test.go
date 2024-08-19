01fd349257617caaad87be1b01f02f2405616c0b
DarrelA <darrelaiscoding@gmail.com>
2024-08-19 13:02:48 +0800

    `SpyBlindAlerter` & add new test case to schedule printing of blind values

--- chris_james/03-build-an-app/05-time/cli_test.go
+++ chris_james/03-build-an-app/05-time/cli_test.go
@@ -3,25 +3,52 @@ package poker_test
 import (
 	"strings"
 	"testing"
+	"time"
 
-	poker "github.com/DarrelASandbox/playground-go/chris_james/03-build-an-app/cli-pkg-struct"
+	poker "github.com/DarrelASandbox/playground-go/chris_james/03-build-an-app/time"
 )
 
+type SpyBlindAlerter struct {
+	alerts []struct {
+		scheduleAt time.Duration
+		amount     int
+	}
+}
+
+func (s *SpyBlindAlerter) ScheduleAlertAt(duration time.Duration, amount int) {
+	s.alerts = append(s.alerts, struct {
+		scheduleAt time.Duration
+		amount     int
+	}{duration, amount})
+}
+
+var dummySpyAlerter = &SpyBlindAlerter{}
+
 func TestCLI(t *testing.T) {
+	playerStore := &poker.StubPlayerStore{}
+	blindAlerter := &SpyBlindAlerter{}
+
 	t.Run("record chris win from user input", func(t *testing.T) {
 		in := strings.NewReader("Chris wins\n")
-		playerStore := &poker.StubPlayerStore{}
-
-		cli := poker.NewCLI(playerStore, in)
+		cli := poker.NewCLI(playerStore, in, dummySpyAlerter)
 		cli.PlayPoker()
 		poker.AssertPlayerWin(t, playerStore, "Chris")
 	})
 
 	t.Run("record chris win from user input", func(t *testing.T) {
 		in := strings.NewReader("Cleo wins\n")
-		playerStore := &poker.StubPlayerStore{}
-		cli := poker.NewCLI(playerStore, in)
+		cli := poker.NewCLI(playerStore, in, dummySpyAlerter)
 		cli.PlayPoker()
 		poker.AssertPlayerWin(t, playerStore, "Cleo")
+	})
+
+	t.Run("it schedules printing of blind values", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
+		cli := poker.NewCLI(playerStore, in, blindAlerter)
+		cli.PlayPoker()
+
+		if len(dummySpyAlerter.alerts) != 1 {
+			t.Fatal("expected a blind alert alert to be scheduled")
+		}
 	})
 }
