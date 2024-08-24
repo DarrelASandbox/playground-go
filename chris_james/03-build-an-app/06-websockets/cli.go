b4a273317349a2a3e8665c9d536215692833b0d3
DarrelA <darrelaiscoding@gmail.com>
2024-08-24 12:24:02 +0800

    `06-websockets`: Starter files from previous section

---
+++ chris_james/03-build-an-app/06-websockets/cli.go
@@ -0,0 +1,66 @@
+package poker
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+)
+
+const (
+	PlayerPrompt         = "Please enter the number of players: "
+	BadPlayerInputErrMsg = "Bad value received for number of players, please try again with a number"
+	BadWinnerInputMsg    = "invalid winner input, expect format of 'PlayerName wins"
+)
+
+type Game interface {
+	Start(numberOfPlayers int)
+	Finish(winner string)
+}
+
+// Switch to `bufio.Scanner` instead of a reader as it's now automatically wrapped at construction time
+type CLI struct {
+	in   *bufio.Scanner
+	out  io.Writer
+	game Game
+}
+
+func NewCLI(in io.Reader, out io.Writer, game Game) *CLI {
+	return &CLI{in: bufio.NewScanner(in), out: out, game: game}
+}
+
+func (cli *CLI) PlayPoker() {
+	fmt.Fprint(cli.out, PlayerPrompt)
+
+	numberOfPlayers, err := strconv.Atoi(cli.readLine())
+	if err != nil {
+		fmt.Fprint(cli.out, BadPlayerInputErrMsg)
+		return
+	}
+
+	fmt.Printf("numberOfPlayers: %v", numberOfPlayers)
+
+	cli.game.Start(numberOfPlayers)
+	winnerInput := cli.readLine()
+	winner, err := extractWinner(winnerInput)
+	if err != nil {
+		fmt.Fprint(cli.out, BadWinnerInputMsg)
+		return
+	}
+
+	cli.game.Finish(winner)
+}
+
+func (cli *CLI) readLine() string {
+	cli.in.Scan()
+	return cli.in.Text()
+}
+
+func extractWinner(userInput string) (string, error) {
+	if !strings.Contains(userInput, " wins") {
+		return "", errors.New(BadWinnerInputMsg)
+	}
+	return strings.Replace(userInput, " wins", "", 1), nil
+}
---
+++ chris_james/03-build-an-app/06-websockets/cli_test.go
@@ -0,0 +1,119 @@
+package poker_test
+
+import (
+	"bytes"
+	"io"
+	"strings"
+	"testing"
+
+	poker "github.com/DarrelASandbox/playground-go/chris_james/03-build-an-app/websockets"
+)
+
+var dummyBlindAlerter = &poker.SpyBlindAlerter{}
+var dummyPlayerStore = &poker.StubPlayerStore{}
+var dummyStdOut = &bytes.Buffer{}
+
+type GameSpy struct {
+	StartCalled      bool
+	StartCalledWith  int
+	FinishedCalled   bool
+	FinishCalledWith string
+}
+
+func (g *GameSpy) Start(numberOfPlayers int) {
+	g.StartCalledWith = numberOfPlayers
+}
+
+func (g *GameSpy) Finish(winner string) {
+	g.FinishCalledWith = winner
+}
+
+func TestCLI(t *testing.T) {
+	t.Run("start game with 3 players and finish game with 'Chris as winner", func(t *testing.T) {
+		in := userSends("3", "Chris wins")
+		stdout := &bytes.Buffer{}
+		game := &GameSpy{}
+		cli := poker.NewCLI(in, stdout, game)
+		cli.PlayPoker()
+		assertMessagesSentToUser(t, stdout, poker.PlayerPrompt)
+		assertGameStartedWith(t, game, 3)
+		assertFinishCalledWith(t, game, "Chris")
+	})
+
+	t.Run("start game with 8 players and record 'Cleo' as winner", func(t *testing.T) {
+		in := userSends("8", "Cleo wins")
+		game := &GameSpy{}
+		cli := poker.NewCLI(in, dummyStdOut, game)
+		cli.PlayPoker()
+		assertGameStartedWith(t, game, 8)
+		assertFinishCalledWith(t, game, "Cleo")
+	})
+
+	t.Run("it prints an error when a non numeric value is entered and does not start the game", func(t *testing.T) {
+		in := strings.NewReader("Pies\n")
+		stdout := &bytes.Buffer{}
+		game := &GameSpy{}
+		cli := poker.NewCLI(in, stdout, game)
+		cli.PlayPoker()
+		assertGameNotStarted(t, game)
+		assertMessagesSentToUser(t, stdout, poker.PlayerPrompt, poker.BadPlayerInputErrMsg)
+	})
+
+	t.Run("it prints an error when the winner is declared incorrectly", func(t *testing.T) {
+		in := userSends("8", "Lloyd is a killer")
+		stdout := &bytes.Buffer{}
+		game := &GameSpy{}
+		cli := poker.NewCLI(in, stdout, game)
+		cli.PlayPoker()
+		assertGameNotFinished(t, game)
+		assertMessagesSentToUser(t, stdout, poker.PlayerPrompt, poker.BadWinnerInputMsg)
+	})
+}
+
+func userSends(messages ...string) io.Reader {
+	return strings.NewReader(strings.Join(messages, "\n"))
+}
+
+func assertScheduledAlert(t testing.TB, got, want poker.ScheduledAlert) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
+}
+
+func assertGameStartedWith(t testing.TB, game *GameSpy, numberOfPlayersWanted int) {
+	t.Helper()
+	if game.StartCalledWith != numberOfPlayersWanted {
+		t.Errorf("wanted Start called with %d but got %d", numberOfPlayersWanted, game.StartCalledWith)
+	}
+}
+
+func assertGameNotStarted(t testing.TB, game *GameSpy) {
+	t.Helper()
+	if game.StartCalled {
+		t.Errorf("game should not have started")
+	}
+}
+
+func assertGameNotFinished(t testing.TB, game *GameSpy) {
+	t.Helper()
+	if game.FinishedCalled {
+		t.Errorf("game should not have finished")
+	}
+}
+
+func assertFinishCalledWith(t testing.TB, game *GameSpy, winner string) {
+	t.Helper()
+	if game.FinishCalledWith != winner {
+		t.Errorf("expected finish called with %q but got %q", winner, game.FinishCalledWith)
+	}
+}
+
+func assertMessagesSentToUser(t testing.TB, stdout *bytes.Buffer, messages ...string) {
+	t.Helper()
+	want := strings.Join(messages, "")
+	got := stdout.String()
+	if got != want {
+		t.Errorf("got %q sent to stdout but expected %+v", got, messages)
+	}
+}
