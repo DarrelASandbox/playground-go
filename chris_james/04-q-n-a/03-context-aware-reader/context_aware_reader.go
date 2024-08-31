160a527b8d46e4ce62c63baea3cabc88990d8c3d
DarrelA <darrelaiscoding@gmail.com>
2024-08-31 11:06:27 +0800

    Verify that the "normal" reader behavior is not broken

--- chris_james/04-q-n-a/03-context-aware-reader/context_aware_reader.go
+++ chris_james/04-q-n-a/03-context-aware-reader/context_aware_reader.go
@@ -4,13 +4,6 @@ import (
 	"io"
 )
 
-/*
---- FAIL: TestContextAwareReader (0.00s)
-    --- FAIL: TestContextAwareReader/behaves_like_a_normal_reader (0.00s)
-panic: runtime error: invalid memory address or nil pointer dereference [recovered]
-        panic: runtime error: invalid memory address or nil pointer dereference
-[signal SIGSEGV: segmentation violation code=0x2 addr=0x0 pc=0x102a0fd78]
-*/
 func NewCancellableReader(rdr io.Reader) io.Reader {
-	return nil
+	return rdr
 }
